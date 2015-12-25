3e3d5a1a3f5df1122e6811a775a643040746446d
percy <percy.zhang@logicmonitor.com>
2015-12-25 19:55:09 +0800

    update

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -8,6 +8,13 @@ config.plugins = (config.plugins || []).concat([
         }
     }),
 
+    // This minifies not only JavaScript, but also
+    // the templates (with html-minifier) and CSS (with cssnano)!
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    }),
 
     new webpack.optimize.OccurenceOrderPlugin()
 ]);
