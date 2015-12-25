566906b0673a1296e9cefffbe5a6ba29e46ffbf2
percy <percy.zhang@logicmonitor.com>
2015-12-25 17:01:34 +0800

    Initial

---
+++ build/webpack.base.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    entry: {
+        Table: './lib/Table.vue'
+    },
+    output: {
+        path: './dist',
+        publicPath: '/assets/',
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: '[name]'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel!eslint',
+                // make sure to exclude 3rd party code in node_modules
+                exclude: /node_modules/
+            },
+            {
+                // edit this for additional asset file types
+                test: /\.(png|jpg|gif|svg|woff|ttf|eot)$/,
+                loader: 'file',
+                query: {
+                    // inline files smaller then 10kb as base64 dataURL
+                    limit: 10000,
+                    // fallback to file-loader with this naming scheme
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    // vue-loader config:
+    // lint all JavaScript inside *.vue files with ESLint
+    // make sure to adjust your .eslintrc
+    vue: {
+        loaders: {
+            js: 'babel!eslint'
+        }
+    },
+    // configure babel-loader (for both .js and .vue files).
+    // see https://babeljs.io/docs/usage/options/
+    babel: {
+        presets: ['es2015', 'stage-0'],
+        plugins: ['transform-runtime']
+    }
+};
---
+++ lib/Table.vue
@@ -0,0 +1,87 @@
+<style lang="sass" scoped>
+    .lm-table {
+        th, td {
+            border-right: 1px dashed #c3c2c2;
+            padding-left: 10px;
+            padding-right: 5px;
+            text-align: left;
+
+            &:last-child {
+                border-right: none;
+            }
+
+            &.center {
+                text-align: center;
+            }
+        }
+
+        th {
+            border-bottom: 1px solid #ccc;
+            padding-top: 4px;
+            padding-bottom: 4px;
+
+            &, * {
+                font-size: 14px;
+                font-weight: 500;
+                color: #333333;
+                white-space: nowrap;
+            }
+        }
+
+        td {
+            & {
+                font-size: 12px;
+                font-weight: normal;
+                color: #333333;
+            }
+
+            .td-wrapper {
+                display: block;
+                display: -webkit-box;
+                max-height: 90px;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                vertical-align: middle;
+                width: 100%;
+                word-break: break-all;
+                -webkit-line-clamp: 4;
+                -webkit-box-orient: vertical;
+                padding-top: 4px;
+            }
+        }
+
+    }
+
+</style>
+
+<template>
+    <table class="lm-table">
+        <thead>
+            <tr>
+                <th v-for="header in data.header">{{header}}</th>
+            </tr>
+        </thead>
+        <tr v-for="row in data.rows">
+            <td v-for="cell in row">
+                <div class="td-wrapper">
+                    {{cell}}
+                </div>
+            </td>
+        </tr>
+    </table>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            data: {
+                type: Object,
+                default: {
+                    header: [],
+                    rows: []
+                }
+            }
+        }
+    };
+</script>
